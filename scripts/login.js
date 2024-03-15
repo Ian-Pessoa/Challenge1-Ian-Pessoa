3a50032e91b43787c4209abe2ebe2cdc87f5ca33
Ian Pessoa <ian.pessoaa@gmail.com>
2024-03-15 17:37:26 -0300

    validação do login utilizando localStorage

--- scripts/login.js
+++ scripts/login.js
@@ -11,3 +11,25 @@ inputs.forEach((input) => {
     }
   });
 });
+
+let storedUser = JSON.parse(localStorage.getItem('user'));
+
+if(storedUser) {
+  loginForm = document.querySelector('.loginForm');
+
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if(email === storedUser.email && password === storedUser.password) {
+      alert('Login successful!');
+      window.location.href = 'home.html';
+    } else {
+      alert('Incorrect email or password');
+    }
+  })
+} else {
+  alert('No user data found. Please register first.');
+}
