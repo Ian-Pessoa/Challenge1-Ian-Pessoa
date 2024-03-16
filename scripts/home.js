300d7c8095754c8b8238e9f79cc8f68a60df6555
Ian Pessoa <ian.pessoaa@gmail.com>
2024-03-15 22:52:31 -0300

    finalizando função counter

--- scripts/home.js
+++ scripts/home.js
@@ -52,3 +52,17 @@ const showWeatherData = async (city) => {
 }
 
 showWeatherData(city);
+
+let timeToRefresh = document.querySelector('.time-to-refresh');
+timeToRefresh.innerText = 30;
+
+let countdown = setInterval(() => {
+  let timeLeft = parseInt(timeToRefresh.innerText);
+  timeLeft -= 1;
+  timeToRefresh.innerText = timeLeft;
+
+  if (timeLeft === 0) {
+    clearInterval(countdown);
+    window.location.reload();
+  }
+}, 1000);
