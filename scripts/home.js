2c341299c0d2d11ee314eda8862d1159def08d58
Ian Pessoa <ian.pessoaa@gmail.com>
2024-03-15 22:37:10 -0300

    temperatura finalizada

--- scripts/home.js
+++ scripts/home.js
@@ -16,3 +16,39 @@ function updateClockAndDate() {
 setInterval(updateClockAndDate, 1000);
 
 updateClockAndDate();
+
+const apiKey = '52e5fe17bc12736dd5418ec5c99c6140';
+
+const storedUser = JSON.parse(localStorage.getItem('user'));
+const city = storedUser.city;
+
+const cityElement = document.querySelector('.city');
+const temperatureElement = document.querySelector('.temperature');
+
+const getWeatherData = async(city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  return data;
+}
+
+const showWeatherData = async (city) => {
+  try {
+    const data = await getWeatherData(city);
+
+    if (data.name && data.main.temp) {
+      cityElement.innerText = data.name;
+      temperatureElement.innerText = parseInt(data.main.temp) + 'º';
+    } else {
+      cityElement.innerText = 'Cidade não encontrada';
+    }
+  } catch (error) {
+    console.error('Erro ao obter dados meteorológicos:', error);
+    cityElement.innerText = 'Erro ao obter dados meteorológicos';
+    temperatureElement.innerText = '';
+  }
+}
+
+showWeatherData(city);
